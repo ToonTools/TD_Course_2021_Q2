307f0bb5cde357c511db9e018ef6ea22e4715826
chrishcarter <chrishcarter@gmail.com>
2021-05-23 18:43:55 +0100

    Create TD_6-2_makeAndRead_textFile.js

---
+++ TD_6-2_makeAndRead_textFile.js
@@ -0,0 +1,17 @@
+// make a text fileCreatedDate
+
+function makeTextFile(){
+	var userName 	= about.getUserName()
+	var myPath 		= "C:\\Users\\" + userName + "\\Desktop\\"
+	MessageLog.trace("myPath = " + myPath)
+	
+}
+
+
+
+// write to that text file
+
+// read from that text file
+
+// actions should have a message log output
+
