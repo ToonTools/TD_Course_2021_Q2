41fe440dea05dcbec80da9bd09f3357ef33dacd1
chrishcarter <chrishcarter@gmail.com>
2021-05-23 18:01:33 +0100

    Update TD_6-1_controlCenter_listScenes.js

--- TD_6-1_controlCenter_listScenes.js
+++ TD_6-1_controlCenter_listScenes.js
@@ -1,5 +1,7 @@
 //run this from command prompt pointed at a harmony server
 
+var envInput = "TCH_TheCatch"
+
 ControlCentre.printToConsole("\n---started---\n")
 
 var envList = ControlCentre.environments();
@@ -7,26 +9,40 @@ var envList = ControlCentre.environments();
 for (var i = 0 ; i< envList.length ; i++){
     var selEnv = envList[i]
     var selEnv_name = selEnv.name
-    ControlCentre.printToConsole("environment [" + i +"] " + selEnv_name )
 
-    // go through all jobs in this environment
+    // if this environment is the input environment, we will continue
 
-    var selEnv_jobList = ControlCentre.jobs(selEnv)
-    for( var j = 0 ; j < selEnv_jobList.length ; j++ ){
-        
-        var selJob = selEnv_jobList[j]
-        var selJob_name = selJob.name
-        ControlCentre.printToConsole("\tjob [" + j +"] " + selJob_name )
+    if( selEnv_name == envInput ){
+        //do something
+        ControlCentre.printToConsole("env [" + i +"] " + selEnv_name )
 
+        // go through all jobs in this environment
+    
+        var selEnv_jobList = ControlCentre.jobs(selEnv)
+        for( var j = 0 ; j < selEnv_jobList.length ; j++ ){
+            
+            var selJob = selEnv_jobList[j]
+            var selJob_name = selJob.name
+            ControlCentre.printToConsole("\tjob [" + j +"] " + selJob_name )
+    
+            // go through all scenes in this job
+    
+            var selJob_sceneList = ControlCentre.scenes(selJob)
+            for( var k = 0 ; k < selJob_sceneList.length ; k++ ){
+    
+                var selScene = selJob_sceneList[k]
+                var selScene_name = selScene.name
+                ControlCentre.printToConsole("\t\tscene [" + k + "] " +selScene_name )
+    
+            }
+        }
     }
-
+    else{
+        //dont do anything
+        ControlCentre.printToConsole("I don't care about : " + selEnv_name)
+    }
 }
-//Retrieve the entire message log and print it out.  
-//var log = ControlCentre.messageLog(); 
-//ControlCentre.printToConsole(log); 
 
 ControlCentre.printToConsole("\n---finished---\n")
 
-
 // ControlCenter -runScript C:\Github\ToonTools\TD_Course_2021_Q2\TD_6-1_controlCenter_listScenes.js
-
