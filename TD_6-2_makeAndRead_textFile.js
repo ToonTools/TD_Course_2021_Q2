6af9c434faa68eb140372296d20bf479f63e3e16
chrishcarter <chrishcarter@gmail.com>
2021-05-23 18:58:34 +0100

    Update TD_6-2_makeAndRead_textFile.js

--- TD_6-2_makeAndRead_textFile.js
+++ TD_6-2_makeAndRead_textFile.js
@@ -1,9 +1,27 @@
 // make a text fileCreatedDate
 
+var userName 			= about.getUserName()
+var myPath 				= "C:\\Users\\" + userName + "\\Desktop\\"
+var fileName			= "myTextFile.txt"
+var fileCompletePath	= myPath + fileName
+
+
 function makeTextFile(){
-	var userName 	= about.getUserName()
-	var myPath 		= "C:\\Users\\" + userName + "\\Desktop\\"
-	MessageLog.trace("myPath = " + myPath)
+	
+	var myFile = new File(fileCompletePath)
+	if( myFile.exists){
+		MessageLog.trace("myFile exists")
+	}
+	else{
+		MessageLog.trace("MyFile does not exists")
+		
+		myFile.open(FileAccess.WriteOnly)
+		myFile.writeLine("my initial message")
+		myFile.close()
+
+	}
+	
+	MessageLog.trace("the file should be at = " + fileCompletePath)
 	
 }
 
