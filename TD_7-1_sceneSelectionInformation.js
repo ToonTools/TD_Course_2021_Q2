ed1ef6ed05fa4f65f06a3110f59bb7c99216427d
chrishcarter <chrishcarter@gmail.com>
2021-05-29 17:50:20 +0100

    Create TD_7-1_sceneSelectionInformation.js

---
+++ TD_7-1_sceneSelectionInformation.js
@@ -0,0 +1,14 @@
+// get current scene name
+
+// get user selection
+
+// return current scene name & user selection
+
+
+function getSceneSelectionInformation(){
+	
+	var sceneName = scene.currentScene()
+		
+	MessageLog.trace("sceneName : " + sceneName) 
+}
+
