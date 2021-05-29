ccb342a065263c388db35928351852ae00cd026d
chrishcarter <chrishcarter@gmail.com>
2021-05-29 19:22:43 +0100

    Create TD_7-3_getAttributes.js

---
+++ TD_7-3_getAttributes.js
@@ -0,0 +1,11 @@
+function getAttributes(){
+
+	var selNode 		= selection.selectedNode(0)
+	var attrNames 	= node.getAllAttrNames(selNode)
+
+	MessageLog.trace(attrNames.length + " attributes found for : " + selNode )
+
+	for( n in attrNames ){
+		MessageLog.trace("\t" + n + " : " + attrNames[n])
+	}
+}
