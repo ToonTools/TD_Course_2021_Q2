e1468b4b7efab07236c0c16693853edf5af21eea
chrishcarter <chrishcarter@gmail.com>
2021-05-29 18:44:14 +0100

    Update TD_7-2_listWriteNodesInScene.js

--- TD_7-2_listWriteNodesInScene.js
+++ TD_7-2_listWriteNodesInScene.js
@@ -3,14 +3,13 @@ function consoleWrite(message){
 	System.println(message)
 }
 
-function getSceneSelectionInformation(){
+function listWriteNodesInScene(){
 	
 	var sceneName 	= scene.currentScene()
 	var projectPath = scene.currentProjectPath()
 	
     //TODO make this tidyer
-	consoleWrite("sceneName : " 	+ sceneName) 
-	consoleWrite("projectPath : " 	+ projectPath) 
+    consoleWrite("\n" + sceneName + "\t" + projectPath)
 	
 	var writeNodes = node.getNodes(["WRITE"])
     
@@ -18,8 +17,17 @@ function getSceneSelectionInformation(){
         consoleWrite("\t " + n + " : " +writeNodes[n] )
     }
 
+    consoleWrite("\n")
+
 }
 
+listWriteNodesInScene()
 
-getSceneSelectionInformation()
+//for harmony standalone
+//HarmonyPremium -compile "C:\Github\ToonTools\TD_Course_2021_Q2\TD_7-2_listWriteNodesInScene.js" "C:\Users\chris\Documents\BlueZoo\testHarmony\colourSpaceTest\colourSpaceTest.xstage"
+
+
+//for control center
+// ControlCenter -runStageScript "C:\Github\ToonTools\TD_Course_2021_Q2\TD_7-2_listWriteNodesInScene.js" -env TCH_TheCatch -job TCH_101_Pilot
+
 
