e4b772515033dba40bd6d3e36a735f4a6cca34e6
chrishcarter <chrishcarter@gmail.com>
2021-05-29 18:04:10 +0100

    added loop to go through selected nodes

--- TD_7-1_sceneSelectionInformation.js
+++ TD_7-1_sceneSelectionInformation.js
@@ -12,5 +12,22 @@ function getSceneSelectionInformation(){
 		
 	MessageLog.trace("sceneName : " 	+ sceneName) 
 	MessageLog.trace("projectPath : " 	+ projectPath) 
+	
+	var selTotal = selection.numberOfNodesSelected()
+	if( selTotal <= 0 ){
+		MessageLog.trace("Nothing is selected") 
+	}
+	
+	/*  slightly briefer way to write the same if statement
+	if( selection.numberOfNodesSelected() <= 0 ){
+		MessageLog.trace("Nothing is selected") 
+	}
+	*/
+	
+	for( var n = 0; n < selTotal; n++ ){
+		var selNode = selection.selectedNode(n)
+		MessageLog.trace("Node "+ n + " : " + selNode)
+	}
+
 }
 
