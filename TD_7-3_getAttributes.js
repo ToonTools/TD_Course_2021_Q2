476d77c1472c8309406b03ad3d9d2df15b1ab482
chrishcarter <chrishcarter@gmail.com>
2021-05-29 19:43:29 +0100

    Created Add composite function

--- TD_7-3_getAttributes.js
+++ TD_7-3_getAttributes.js
@@ -1,11 +1,33 @@
 function getAttributes(){
 
 	var selNode 		= selection.selectedNode(0)
-	var attrNames 	= node.getAllAttrNames(selNode)
+	
+/*
+	var writePath = node.getAttr(selNode, 1,  "Movie path")
+
+	MessageLog.trace(writePath)
+
+	*/
+
+/*
+	var writePath = node.getTextAttr( selNode, 1, "MOVIE PATH")
+
+MessageLog.trace(writePath)
+	*/
+
+
+	
+
+
+var attrNames 	= node.getAllAttrNames(selNode)
 
 	MessageLog.trace(attrNames.length + " attributes found for : " + selNode )
 
+
+
+	
 	for( n in attrNames ){
 		MessageLog.trace("\t" + n + " : " + attrNames[n])
 	}
+
 }
---
+++ TD_7-4_addComposite.js
@@ -0,0 +1,32 @@
+function addComposite(){
+
+	MessageLog.trace("add composite function called")
+
+
+	if( selection.numberOfNodesSelected()<= 0){
+		MessageLog.trace("can't do anythign with no nodes selected")	
+		return
+	}
+	else if ( selection.numberOfNodesSelected() > 1){
+		MessageLog.trace("I can't handle more than one node selected yet")	
+		return
+	}
+
+	// user selection
+	var selNode = selection.selectedNode(0)
+	MessageLog.trace("selNode = " + selNode)
+
+
+	// reject if more than one node is selected
+
+	// create a composite
+
+	// name composite
+
+
+	// link composite to drawing node
+
+	// move composite to good location
+
+
+}
