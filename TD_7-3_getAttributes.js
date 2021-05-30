b0b43f8d3a0aeb90c06d0016519a02e0385e272a
chrishcarter <chrishcarter@gmail.com>
2021-05-30 17:20:23 +0100

    accessing the attribute value

--- TD_7-3_getAttributes.js
+++ TD_7-3_getAttributes.js
@@ -1,15 +1,17 @@
 function getAttributes(){
 
-	//applies to first selected node
-	var selNode 		= selection.selectedNode(0)
-	
+	var selNode 			= selection.selectedNode(0)
 	var attrNames 		= node.getAllAttrNames(selNode)
-
+	var attrKeyword	= node.getAllAttrKeywords(selNode)
+	var frame				= 1
 
 	MessageLog.trace(attrNames.length + " attributes found for : " + selNode )
 	
 	for( n in attrNames ){
-		MessageLog.trace("\t" + n + " : " + attrNames[n])
+
+		var attrVal = node.getTextAttr(selNode,frame,attrKeyword[n])
+
+		MessageLog.trace("\t" + n + " : " + attrNames[n] + " : " + attrKeyword[n] + " : " + attrVal)
 	}
 
 }
