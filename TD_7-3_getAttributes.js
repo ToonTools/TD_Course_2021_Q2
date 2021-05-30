92d65565caa13a8680b0c5b2af700a06681d8552
chrishcarter <chrishcarter@gmail.com>
2021-05-30 17:36:15 +0100

    added ways to find x,y,z sub attributes

--- TD_7-3_getAttributes.js
+++ TD_7-3_getAttributes.js
@@ -11,8 +11,28 @@ function getAttributes(){
 
 		var attrVal = node.getTextAttr(selNode,frame,attrKeyword[n])
 
-		//MessageLog.trace("\t" + n + " : " + attrNames[n] + " : " + attrKeyword[n] + " : " + attrVal)
 		MessageLog.trace('\t[ ' + attrVal + ' ]\t ' + attrNames[n] + ' (' + attrKeyword[n] + ') ')
+
+		if( attrVal == ""){
+
+			var attrKeyword_x = attrKeyword[n] + ".x"
+			var subAttrVal_x = node.getTextAttr(selNode,frame,attrKeyword_x)
+			if(subAttrVal_x != ""){
+				MessageLog.trace('\t  [' + subAttrVal_x + ']\t ' + attrKeyword_x)
+			}
+
+			var attrKeyword_y = attrKeyword[n] + ".y"
+			var subAttrVal_y = node.getTextAttr(selNode,frame,attrKeyword_y)
+			if(subAttrVal_y != ""){
+				MessageLog.trace('\t  [' + subAttrVal_y + ']\t ' + attrKeyword_y)
+			}
+
+			var attrKeyword_z = attrKeyword[n] + ".z"
+			var subAttrVal_z = node.getTextAttr(selNode,frame,attrKeyword_z)
+			if(subAttrVal_z != ""){
+				MessageLog.trace('\t  [' + subAttrVal_z + ']\t ' + attrKeyword_z)
+			}
+		}
 
 
 	}
