e6a74cba69e39030a15c5885a8894a7b3006ea5e
chrishcarter <chrishcarter@gmail.com>
2021-05-30 17:14:18 +0100

    WIP

--- TD_7-3_getAttributes.js
+++ TD_7-3_getAttributes.js
@@ -1,30 +1,12 @@
 function getAttributes(){
 
+	//applies to first selected node
 	var selNode 		= selection.selectedNode(0)
 	
-/*
-	var writePath = node.getAttr(selNode, 1,  "Movie path")
+	var attrNames 		= node.getAllAttrNames(selNode)
 
-	MessageLog.trace(writePath)
-
-	*/
-
-/*
-	var writePath = node.getTextAttr( selNode, 1, "MOVIE PATH")
-
-MessageLog.trace(writePath)
-	*/
-
-
-	
-
-
-var attrNames 	= node.getAllAttrNames(selNode)
 
 	MessageLog.trace(attrNames.length + " attributes found for : " + selNode )
-
-
-
 	
 	for( n in attrNames ){
 		MessageLog.trace("\t" + n + " : " + attrNames[n])
