4bd0b6b813cc2da74656599f1f86e384e1bcb55a
chrishcarter <chrishcarter@gmail.com>
2021-06-05 19:34:15 +0100

    update name of ui file

--- TD_2021_Q2_customWindow/view.js
+++ TD_2021_Q2_customWindow/view.js
@@ -6,7 +6,7 @@ function initView(){
         scriptedView.minimumWidth   = UiLoader.dpiScale(375)
         scriptedView.setPreferredLayoutSize(UiLoader.dpiScale(320),UiLoader.dpiScale(375))
 
-        var ui = ScriptManager.loadViewUI(scriptedView, "./customWindow.ui")
+        var ui = ScriptManager.loadViewUI(scriptedView, "./TD_2021_Q2_customWindow.ui")
 
         var customFunction_1 = function(){
             MessageBox.information("this is the function for button 1")
