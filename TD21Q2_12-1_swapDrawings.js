854913493eaf83d8e3dfd842e3e0993ac7e11ea9
chrishcarter <chrishcarter@gmail.com>
2021-06-13 18:00:53 +0100

    Update TD21Q2_12-1_swapDrawings.js

--- TD21Q2_12-1_swapDrawings.js
+++ TD21Q2_12-1_swapDrawings.js
@@ -2,6 +2,25 @@ function swapDrawing(){
     scene.beginUndoRedoAccum("swapDrawing()")
     MessageLog.trace("\n\n --- swapDrawing() called --- ")
     
+    // select every drawing node in the scene
+    var readNodes = node.getNodes(["READ"])
+
+    for ( var i = 0 ; i < readNodes.length ; i++ ){
+        var sel_readNode = readNodes[i]
+        MessageLog.trace("current read node = " + sel_readNode)
+
+    }
+
+
+    // for each node figure out what the name of current drawing is
+    // // if the drawing name is REJECT then see if we can REPLACE it
+    // // // if we can replace it then lets do so
+
+
+    
+
+
+
 
 
     MessageLog.trace("\n --- swapDrawing() completed successfully --- \n")
