c92ded765929a9fa41835e494759936d1aa0b4b0
chrishcarter <chrishcarter@gmail.com>
2021-06-13 17:55:04 +0100

    created swapDrawings function

--- TD21Q2_11-2_selectionInformation.js
+++ TD21Q2_11-2_selectionInformation.js
@@ -29,4 +29,3 @@ function getSelectionInformation(){
     }
 	return
 }
-
---
+++ TD21Q2_12-1_swapDrawings.js
@@ -0,0 +1,9 @@
+function swapDrawing(){
+    scene.beginUndoRedoAccum("swapDrawing()")
+    MessageLog.trace("\n\n --- swapDrawing() called --- ")
+    
+
+
+    MessageLog.trace("\n --- swapDrawing() completed successfully --- \n")
+    scene.endUndoRedoAccum()
+}
