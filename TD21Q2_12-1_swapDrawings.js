9cff103d97701999ff76b6be7dc0a16a266d162f
chrishcarter <chrishcarter@gmail.com>
2021-06-13 18:10:28 +0100

    list drawings on frame

--- TD21Q2_12-1_swapDrawings.js
+++ TD21Q2_12-1_swapDrawings.js
@@ -1,18 +1,31 @@
 function swapDrawing(){
     scene.beginUndoRedoAccum("swapDrawing()")
     MessageLog.trace("\n\n --- swapDrawing() called --- ")
+
+    var shotStart   = 1
+    var shotEnd     = frame.numberOf();
     
     // select every drawing node in the scene
     var readNodes = node.getNodes(["READ"])
 
     for ( var i = 0 ; i < readNodes.length ; i++ ){
-        var sel_readNode = readNodes[i]
+        var sel_readNode    = readNodes[i]
+        var sel_column      = node.linkedColumn(sel_readNode, "DRAWING.ELEMENT")
+
         MessageLog.trace("current read node = " + sel_readNode)
 
+        for( var f = shotStart ; f < shotEnd ; f++){
+            var sel_drawing = column.getEntry(sel_column, 1, f)
+
+            MessagLog.trace("\tf-" + f + " : " + sel_drawing)
+        }
+
+        // for each node figure out what the name of current drawing is
+        
     }
 
 
-    // for each node figure out what the name of current drawing is
+    
     // // if the drawing name is REJECT then see if we can REPLACE it
     // // // if we can replace it then lets do so
 
